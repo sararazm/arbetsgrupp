732b5f1bc4cf4832d67c37f689628711819ffc11
Erik <eriksodermanolo@outlook.com>
2022-08-29 02:05:18 +0200

    Fixed a Sign in page, Added React-Router-Dom etc.

--- client/quiz-app/src/App.js
+++ client/quiz-app/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/Signup" element={<Signup/>}/>
+        
+      </Routes>
+    </Router>
   );
 }
 
